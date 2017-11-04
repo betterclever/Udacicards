6d816ca5981ad0c995e12d32c18e8fc10b2494f6
betterclever <paliwal.pranjal83@gmail.com>
2017-11-04 17:13:01 +0530

    Added Typings Attribution :)

--- typings/expo/index.d.ts
+++ typings/expo/index.d.ts
@@ -1,3 +1,4 @@
+// Typings from https://gist.github.com/pierre-H/eef9a9225fb1c5a0f81180a8b0fbb2c2
 declare module 'expo' {
     import { EventSubscription } from 'fbemitter';
     import { ViewProperties, ViewStyle } from 'react-native';
